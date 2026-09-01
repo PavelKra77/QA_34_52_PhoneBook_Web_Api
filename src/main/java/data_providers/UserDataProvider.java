8ce7f55a518fa226141bb3d38e24db6c74ccdf58
PavelKra77 <pav.kravets86@gmail.com>
2026-09-01 17:44:30 +0300

    Logger

--- src/main/java/data_providers/UserDataProvider.java
+++ src/main/java/data_providers/UserDataProvider.java
@@ -1,15 +1,3 @@
-// @DataProvider — спецаннотация TestNG. Она обозначает, что этот метод поставляет тестовые данные.
-//Iterator<UserLombok> — формат возвращаемого значения. TestNG автоматически запустит ваш @Test столько раз,
-// сколько объектов будет находиться в этом Iterator.
-//FileReader умеет читать только отдельные символы или массивы символов. У него нет метода readLine().
-//BufferedReader через readLine() читает текст от начала и до символа переноса строки (\n или \r\n).
-// 1. FileReader открывает соединение с файлом на диске
-// FileReader fileReader = new FileReader("src/test/resources/data.csv");
-// 2. BufferedReader оборачивает fileReader, добавляя буфер и метод readLine()
-// BufferedReader bufferedReader = new BufferedReader(fileReader);
-// 3. Теперь можно удобно читать файл построчно с высокой скоростью
-// String line = bufferedReader.readLine();
-
 package data_providers;
 
 import dto.UserLombok;
@@ -30,7 +18,7 @@ public class UserDataProvider {
                 (new FileReader("src/test/resources/wrong_email_password .csv"))){
             String line =bufferedReader.readLine();
             while (line != null){
-                String[] splitLine = line.split(",");  //line.split(",") разбивает строку на массив из двух элементов по запятой.
+                String[] splitLine = line.split(",");
                 list.add(UserLombok.builder()
                         .username(splitLine[0])
                         .password(splitLine[1])
@@ -41,7 +29,40 @@ public class UserDataProvider {
             e.printStackTrace();
             System.out.println("created exception");
         }
-        return list.listIterator();
-
-    }
+        return list.listIterator();    }
 }
+
+// @DataProvider — спецаннотация TestNG. Она обозначает, что этот метод готовит данные для автотестов.
+//1. Iterator<UserLombok> — Заголовок метода обещает: Я верну указатель (Iterator), который выдает объекты UserLombok.
+// TestNG автоматически запустит ваш @Test столько раз,
+// сколько объектов будет находиться в этом Iterator. На выходе метод отдаст список пользователей,
+// по которому тест сможет прошвырнуться по очереди (перебрать их через Iterator).
+//2. List<UserLombok>  Создаем пустой список list, куда будем складывать прочитанных из файла пользователей.
+//С использованием <UserLombok> компилятор гарантирует, что в коллекцию попадают только объекты класса UserLombok
+//ArrayList массив. он автоматически растягивается, когда вы добавляете новые элементы
+//Обычный список (List) — это просто коробка, где лежат готовые объекты.
+//Iterator — это указатель (палец), который умеет шагать по этой коробке от первого элемента к последнему.
+//3. FileReader открывает файл с данными "src/test/re..,
+// Он умеет читать только отдельные символы или массивы символов. У него нет метода readLine().
+// 4. BufferedReader оборачивает fileReader, чтобы читать его не по одной букве, а целыми строками.
+// через readLine() читает текст от начала и до символа переноса строки (\n или \r\n).
+// объект bufferedReader — это  читатель со сдвигающимся курсором.Он запоминает место, на котором остановился в предыдущий раз.
+// 5. String line = bufferedReader.readLine(); Читаем самую первую строчку из файла и сохраняем ее в переменную line
+// 6. Цикл while читает файл построчно, пока строчки не закончатся. Берет строку вида user@test.com,12345.
+// 7. line.split(",") разбивает строку на 2элемента по запятой. логин (splitLine[0]) и пароль (splitLine[1]).
+// 8. Создаем нового пользователя (объект UserLombok)и добавляем в наш список list, заполняем его логином и паролем под индексом 0...
+// 9. line = bufferedReader.readLine Читаем следующую строчку файла. Цикл повторяется заново для новой строки.
+// 10. return list.listIterator();используется для того, чтобы передать подготовленные тестовые данные в TestNG.
+// list.listIterator(), создали «указатель», чтобы фреймворк тестов мог постепенно брасывать данные в тест по одному пользователю.
+
+// Iterator<UserLombok> в заголовке —  обещание метода «Этот метод в итоге отдаст итератор (переборщик,закладку), который по очереди выдает объекты типа UserLombok».
+//list.listIterator() в конце —выполнение обещания. Сам list ( return list) вернуть нельзя, потому что заголовок требует Iterator.
+// Поэтому ты вызываешь метод .listIterator() у списка, который превращает список в нужный формат.
+//return list.listIterator()  отдаёт фреймворку TestNG специальную «закладку» (итератор), указывающую на начало списка list.
+
+// String line = bufferedReader.readLine():Метод берет самую первую строчку: "user1@test.com,pass1".
+//Кладет её в переменную line =  "user1@test.com,pass1". BufferedReader запоминает: «Я остановился на конце 1-й строки».
+// в конце цикла while Вызывается line = bufferedReader.readLie.
+// BufferedReader помнит, где остановился, двигается дальше и считывает 2-ю строку, Переменная line перезаписывается
+// Когда ты создаешь new BufferedReader(...), Java открывает файл и ставит курсор (закладку) в самое начало — перед первой строкой.
+//Первый вызов bufferedReader.readLine() считывает символы до ближайшего переноса строки (Enter), отдает их тебе и сдвигает внутренний курсор на начало 2-й строки.
